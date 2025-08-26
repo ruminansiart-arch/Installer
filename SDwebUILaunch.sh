a179b498b32bc794766fb4aa9691e344ef60f1ca
ruminansiart-arch <ruminansiart@gmail.com>
2025-08-27 03:11:47 +0700

    Update SDwebUILaunch.sh

--- SDwebUILaunch.sh
+++ SDwebUILaunch.sh
@@ -3,6 +3,6 @@
 # Activate Conda environment
 source /workspace/ENVS/Conda_P3.10/bin/activate
 
-# Navigate and launch
+# Change directory and launch
 cd /workspace/stable-diffusion-webui
-python launch.py --listen --port 8288 --theme dark
+exec python launch.py --listen --port 8288 --theme dark
